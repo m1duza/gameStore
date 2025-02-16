519ba10305a5dde42274164506254c94797b134d
m1duza <ivan.zubcov228@gmail.com>
2025-02-16 15:05:09 +0300

    good10 🎉

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-    },
-    host: 'localhost',
-    port: 5173,
+    host: '0.0.0.0', // Разрешает доступ с локальной сети
+    port: 5173, // Оставляем стандартный порт Vite
+    cors: true, // Включает CORS, если вдруг есть проблемы с VK API
   },
-})
+});
